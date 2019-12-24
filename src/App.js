9de87a3644c051b82a9602347afd5a70a134a2ba
Brandon Hills <brandon_hills@icloud.com>
2019-12-24 15:21:36 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import moment from 'moment';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      now: moment()
+     // today: moment().format('l'),
+     // time: moment().format('LTS')
+    };
+  }
+
+  componentDidMount = () => {
+    this.timer = setInterval(
+      () => this.second(),
+      1000
+    );
+  }
+
+  componentWillUnmount =() => {
+    clearInterval(this.timer);
+  }
+
+  second = () => {
+    this.setState({
+      now: moment()
+    });
+  }
+
+  render() {
+   // console.log(this.state.time);
+    return (
+      <div>
+        <h1>Count Down To Christmas</h1>
+        <p>Today is {this.state.now.format('l')} and it is {this.state.now.format('LTS')}</p>
+
+      </div>
+    );
+  }
 }
 
 export default App;
