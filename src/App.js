a2047571812429e471a7689c8add4e71c77cda23
Brandon Hills <brandon_hills@icloud.com>
2020-02-13 11:15:06 -0500

    Added coundownYear incrementing logic, working on displaying difference in time till Christmas Day

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      now: moment()
+      now: moment(),
+      currentDay: moment().format('DD'),
+      currentMonth: moment().format('MM'),
+      currentYear: moment().format('YYYY'),
+      countdownYear: moment().format('YYYY'),
+      christmasDay: 1225 // MMDD format
      // today: moment().format('l'),
      // time: moment().format('LTS')
     };
   }
-
+  
   componentDidMount = () => {
     this.timer = setInterval(
       () => this.second(),
@@ -27,13 +32,27 @@ class App extends React.Component {
       now: moment()
     });
   }
+  
 
   render() {
-   // console.log(this.state.time);
+    // If after Christmas Day, increment currentYear so the time starts a new countdown
+    if ((this.state.now.format('MMDD') > this.state.christmasDay) && this.state.countdownYear <= this.state.now.format('YYYY'))  {
+      var newYear = (Number(this.state.countdownYear) + 1);
+      this.setState({
+        countdownYear: newYear
+      });
+    };
+
+    var today = moment([2020, 2, 13]);
+    var christmasDay = moment([2020, 12, 25]);
+    console.log(today);
+    console.log(christmasDay);
+
     return (
       <div>
         <h1>Count Down To Christmas</h1>
-        <p>Today is {this.state.now.format('l')} and it is {this.state.now.format('LTS')}</p>
+        <p>Today is {this.state.now.format('LL')} and it is {this.state.now.format('LTS')}</p>
+    <p>Christmas {this.state.countdownYear} is in {christmasDay.diff(today)}</p>
 
       </div>
     );
